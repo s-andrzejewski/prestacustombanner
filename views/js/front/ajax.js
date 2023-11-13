5d3a4cd2a3137ab17c94251cd71952528f952efc
s-andrzejewski <s.andrzejewski.7@gmail.com>
2023-11-13 14:45:14 +0100

    Better formatting

--- views/js/front/ajax.js
+++ views/js/front/ajax.js
@@ -4,31 +4,31 @@ const data = "action=get_message";
 fetch(ajaxUrl, {
     method: "POST",
     headers: {
-      "Content-Type": "application/json",
+        "Content-Type": "application/json",
     },
     body: JSON.stringify({ action: data }),
 })
     .then((response) => {
         if (response.ok) {
-        return response.json();
+            return response.json();
         } else {
-        throw new Error(`HTTP error! Status: ${response.status}`);
+            throw new Error(`HTTP error! Status: ${response.status}`);
         }
     })
     .then((response) => {
         if (response.status) {
-          const container = document.querySelector(".presta-custom-message__ajax-response");
-          container.classList.add("skeleton");
-          container.classList.add("skeleton-paragraph");
-    
-          // setTimeout - only for demonstration purposes:
-          setTimeout(() => {
-            if (container) {
-              container.innerHTML = response.message;
-              container.classList.remove("skeleton");
-              container.classList.remove("skeleton-paragraph");
-            }
-          }, 1000);
+            const container = document.querySelector(".presta-custom-message__ajax-response");
+            container.classList.add("skeleton");
+            container.classList.add("skeleton-paragraph");
+
+            // setTimeout - only for demonstration purposes:
+            setTimeout(() => {
+                if (container) {
+                    container.innerHTML = response.message;
+                    container.classList.remove("skeleton");
+                    container.classList.remove("skeleton-paragraph");
+                }
+            }, 1000);
         }
     })
     .catch((error) => {
