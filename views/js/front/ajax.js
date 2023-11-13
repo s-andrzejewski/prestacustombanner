b281cf8dcb876ae2c82d520ec999de9be2d92e9c
s-andrzejewski <s.andrzejewski.7@gmail.com>
2023-11-13 14:31:09 +0100

    xhr to fetch

--- views/js/front/ajax.js
+++ views/js/front/ajax.js
@@ -1,29 +1,37 @@
 const ajaxUrl = ajax_link;
+const data = "action=get_message";
 const xhr = new XMLHttpRequest();
 
-xhr.onload = () => {
-    if (xhr.status >= 200 && xhr.status < 300) {
-        const response = JSON.parse(xhr.responseText);
-        if (response.status) {
-            const container = document.querySelector(".presta-custom-message__ajax-response");
-            container.classList.add("skeleton");
-            container.classList.add("skeleton-paragraph");
-
-            // setTimeout - only for demonstration purposes:
-            setTimeout(()=>{
-                if (container) {
-                    container.innerHTML = response.message;
-                    container.classList.remove("skeleton");
-                    container.classList.remove("skeleton-paragraph");
-                }
-            },1000);
+fetch(ajaxUrl, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ action: data }),
+})
+    .then((response) => {
+        if (response.ok) {
+        return response.json();
+        } else {
+        throw new Error(`HTTP error! Status: ${response.status}`);
         }
-
-    }
-};
-
-xhr.open("POST", ajaxUrl, true);
-const data = "action=get_message";
-
-xhr.setRequestHeader("Content-Type", "application/json");
-xhr.send(data);
+    })
+    .then((response) => {
+        if (response.status) {
+          const container = document.querySelector(".presta-custom-message__ajax-response");
+          container.classList.add("skeleton");
+          container.classList.add("skeleton-paragraph");
+    
+          // setTimeout - only for demonstration purposes:
+          setTimeout(() => {
+            if (container) {
+              container.innerHTML = response.message;
+              container.classList.remove("skeleton");
+              container.classList.remove("skeleton-paragraph");
+            }
+          }, 1000);
+        }
+    })
+    .catch((error) => {
+        console.error("Fetch error:", error);
+    });
