7c802b8ae8a51bc6a53074685bdbaf4e559cf93e
s-andrzejewski <s.andrzejewski.7@gmail.com>
2023-11-13 14:50:48 +0100

    Optimized pl translation file

--- translations/pl.php
+++ translations/pl.php
@@ -1,24 +1,26 @@
 <?php
 
-$_MODULES = array();
-
-$_MODULES['prestacustommessage']['Banner with custom message'] = 'Baner z niestandardową wiadomością.';
-$_MODULES['prestacustommessage']['Displays custom banner on homepage.'] = 'Wyświetla niestandardowy baner na stronie głównej.';
-$_MODULES['prestacustommessage']['Are you sure you want to uninstall this module?'] = 'Czy na pewno chcesz odinstalować ten moduł?';
-$_MODULES['prestacustommessage']['Settings'] = 'Ustawienia';
-$_MODULES['prestacustommessage']['Enter a heading text for the message.'] = 'Wprowadź tekst nagłówka dla wiadomości.';
-$_MODULES['prestacustommessage']['Heading'] = 'Nagłówek';
-$_MODULES['prestacustommessage']['Enter banner\'s content.'] = 'Wprowadź treść banera.';
-$_MODULES['prestacustommessage']['Description'] = 'Opis';
-$_MODULES['prestacustommessage']['Enter color code for text (hex or rgba).'] = 'Wprowadź kod koloru tekstu (hex lub rgba).';
-$_MODULES['prestacustommessage']['Font color:'] = 'Kolor czcionki:';
-$_MODULES['prestacustommessage']['Upload image'] = 'Prześlij obraz';
-$_MODULES['prestacustommessage']['Upload an image for the custom banner.'] = 'Prześlij obraz dla niestandardowego banera.';
-$_MODULES['prestacustommessage']['Button text'] = 'Tekst przycisku';
-$_MODULES['prestacustommessage']['Enter the text for the button.'] = 'Wprowadź tekst przycisku.';
-$_MODULES['prestacustommessage']['Button URL'] = 'Adres URL przycisku';
-$_MODULES['prestacustommessage']['Enter the URL for the button click action.'] = 'Wprowadź adres URL dla akcji kliknięcia przycisku.';
-$_MODULES['prestacustommessage']['Save'] = 'Zapisz';
-$_MODULES['prestacustommessage']['Nothing here.'] = 'Nic tu nie ma.';
-$_MODULES['prestacustommessage']['Ajax request button label'] = 'Etykieta przycisku ładowania treści z AJAXa';
-$_MODULES['prestacustommessage']['Enter content to load from AJAX.'] = 'Wprowadź treść do załadowania przez AJAX.';
+$_MODULES = array(
+    'prestacustommessage' => array(
+        'Banner with custom message' => 'Baner z niestandardową wiadomością.',
+        'Displays custom banner on homepage.' => 'Wyświetla niestandardowy baner na stronie głównej.',
+        'Are you sure you want to uninstall this module?' => 'Czy na pewno chcesz odinstalować ten moduł?',
+        'Settings' => 'Ustawienia',
+        'Enter a heading text for the message.' => 'Wprowadź tekst nagłówka dla wiadomości.',
+        'Heading' => 'Nagłówek',
+        'Enter banner\'s content.' => 'Wprowadź treść banera.',
+        'Description' => 'Opis',
+        'Enter color code for text (hex or rgba).' => 'Wprowadź kod koloru tekstu (hex lub rgba).',
+        'Font color:' => 'Kolor czcionki:',
+        'Upload image' => 'Prześlij obraz',
+        'Upload an image for the custom banner.' => 'Prześlij obraz dla niestandardowego banera.',
+        'Button text' => 'Tekst przycisku',
+        'Enter the text for the button.' => 'Wprowadź tekst przycisku.',
+        'Button URL' => 'Adres URL przycisku',
+        'Enter the URL for the button click action.' => 'Wprowadź adres URL dla akcji kliknięcia przycisku.',
+        'Save' => 'Zapisz',
+        'Nothing here.' => 'Nic tu nie ma.',
+        'Ajax request button label' => 'Etykieta przycisku ładowania treści z AJAXa',
+        'Enter content to load from AJAX.' => 'Wprowadź treść do załadowania przez AJAX.'
+    )
+);
