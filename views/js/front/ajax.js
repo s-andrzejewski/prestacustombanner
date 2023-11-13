91d2bbc0f9487567557dcba81e53544b70807f2b
s-andrzejewski <s.andrzejewski.7@gmail.com>
2023-11-13 15:35:01 +0100

    Improved fetch request

--- views/js/front/ajax.js
+++ views/js/front/ajax.js
@@ -1,36 +1,47 @@
-const ajaxUrl = ajax_link;
-const data = "action=get_message";
+async function fetchAjaxData(ajaxUrl) {
+    try {
+        const data = "action=get_message";
 
-fetch(ajaxUrl, {
-    method: "POST",
-    headers: {
-        "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ action: data }),
-})
-    .then((response) => {
+        const response = await fetch(ajaxUrl, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ action: data }),
+        });
+
         if (response.ok) {
-            return response.json();
-        } else {
-            throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-    })
-    .then((response) => {
-        if (response.status) {
-            const container = document.querySelector(".presta-custom-message__ajax-response");
-            container.classList.add("skeleton");
-            container.classList.add("skeleton-paragraph");
+            const responseData = await response.json();
 
-            // setTimeout - only for demonstration purposes:
-            setTimeout(() => {
-                if (container) {
-                    container.innerHTML = response.message;
-                    container.classList.remove("skeleton");
-                    container.classList.remove("skeleton-paragraph");
-                }
-            }, 1000);
+            if (responseData.status) {
+                const container = document.querySelector(".presta-custom-message__ajax-response");
+                container.classList.add("skeleton");
+                container.classList.add("skeleton-paragraph");
+
+                // setTimeout - only for demonstration purposes:
+                setTimeout(() => {
+                    if (container) {
+                        container.innerHTML = responseData.message;
+                        container.classList.remove("skeleton");
+                        container.classList.remove("skeleton-paragraph");
+                    }
+                }, 1000);
+            }
+        } else {
+            console.error(`HTTP error! Status: ${response.status}`);
+
+            // Handle the HTTP error gracefully, e.g., display an error message to the user
+            const errorMessage = "An error occurred while fetching data. Please try again later.";
+            // You can display the errorMessage in the DOM or use other means to notify the user.
         }
-    })
-    .catch((error) => {
+    } catch (error) {
         console.error("Fetch error:", error);
-    });
+
+        // Handle any other errors gracefully, e.g., display an error message to the user
+        const errorMessage = "An error occurred while fetching data. Please try again later.";
+        // You can display the errorMessage in the DOM or use other means to notify the user.
+    }
+}
+
+const ajaxUrl = ajax_link;
+fetchAjaxData(ajaxUrl);
